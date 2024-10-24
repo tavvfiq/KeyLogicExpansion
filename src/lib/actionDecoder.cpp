684e04a41aa1ff947fbbeb4dfa956b4c0cd2b59e
Acook1e <acfovx@outlook.com>
2024-10-24 16:44:29 +0800

    complete attack call

--- src/lib/actionDecoder.cpp
+++ src/lib/actionDecoder.cpp
@@ -5,6 +5,24 @@ using namespace REL;
 
 namespace ActionDecoder
 {
+
+    typedef bool doAnimation_t(RE::TESActionData *);
+    REL::Relocation<doAnimation_t> doAnimation{RELOCATION_ID(40551, 41557)};
+
+    void Attack(BSFixedString userEvent)
+    {
+        auto action = userEvent;
+        if (userEvent == Var::userEvent->rightAttack)
+            action = Var::userEvent->attackStart;
+        else if (userEvent == Var::userEvent->leftAttack)
+            action = Var::userEvent->blockStart;
+        else if (userEvent == Var::userEvent->attackPowerStart)
+            action = "attackPowerStartH2HCombo";
+        SKSE::GetTaskInterface()->AddTask([action]()
+                                          { Var::player->NotifyAnimationGraph(action); });
+
+        //  Var::pCtrl->data.moveInputVec = NiPoint2(1.0, 1.0);
+    }
     void ClickAction(BSFixedString userEvent, ActionType type)
     {
         logger::trace("UserEvent: {}     Type: {}", userEvent.c_str(), (int)type);
@@ -14,10 +32,7 @@ namespace ActionDecoder
             Hook::MenuOpenHandler::PB(RE::ButtonEvent::Create(INPUT_DEVICE::kKeyboard, userEvent, 0, 1, 0));
             break;
         case ActionType::AttackBlock:
-            //if (userEvent == Var::userEvent->rightAttack)
-                //Hook::AttackBlockHandler::PB(RE::ButtonEvent::Create(INPUT_DEVICE::kMouse, userEvent, 0, 256, 0), &Var::pCtrl->data);
-            // else if (userEvent == Var::userEvent->attackPowerStart)
-            // Hook::AttackBlockHandler::PB(RE::ButtonEvent::Create(INPUT_DEVICE::kMouse, userEvent, 0, 256, 1), &Var::pCtrl->data);
+            Attack(userEvent);
             break;
         }
     }
